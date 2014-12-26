ecc373fda5c5df68eeb9cf4bbd61dedb3c41aeea
taukir4u <taukir4u@gmail.com>
2014-12-26 19:35:00 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ Convert App/ConvertAppUI.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Convert_App
+{
+    partial class ConvertAppUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numberOfTicketsTextBox = new System.Windows.Forms.TextBox();
+            this.customerNameTextBox = new System.Windows.Forms.TextBox();
+            this.detailsButton = new System.Windows.Forms.Button();
+            this.totalPurchaseButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numberOfTicketsTextBox);
+            this.groupBox1.Controls.Add(this.customerNameTextBox);
+            this.groupBox1.Controls.Add(this.detailsButton);
+            this.groupBox1.Controls.Add(this.totalPurchaseButton);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(47, 30);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(571, 256);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // numberOfTicketsTextBox
+            // 
+            this.numberOfTicketsTextBox.Location = new System.Drawing.Point(141, 98);
+            this.numberOfTicketsTextBox.Name = "numberOfTicketsTextBox";
+            this.numberOfTicketsTextBox.Size = new System.Drawing.Size(188, 20);
+            this.numberOfTicketsTextBox.TabIndex = 6;
+            // 
+            // customerNameTextBox
+            // 
+            this.customerNameTextBox.Location = new System.Drawing.Point(141, 29);
+            this.customerNameTextBox.Name = "customerNameTextBox";
+            this.customerNameTextBox.Size = new System.Drawing.Size(188, 20);
+            this.customerNameTextBox.TabIndex = 5;
+            // 
+            // detailsButton
+            // 
+            this.detailsButton.Location = new System.Drawing.Point(324, 191);
+            this.detailsButton.Name = "detailsButton";
+            this.detailsButton.Size = new System.Drawing.Size(75, 23);
+            this.detailsButton.TabIndex = 4;
+            this.detailsButton.Text = "Details";
+            this.detailsButton.UseVisualStyleBackColor = true;
+            this.detailsButton.Click += new System.EventHandler(this.detailsButton_Click);
+            // 
+            // totalPurchaseButton
+            // 
+            this.totalPurchaseButton.Location = new System.Drawing.Point(91, 191);
+            this.totalPurchaseButton.Name = "totalPurchaseButton";
+            this.totalPurchaseButton.Size = new System.Drawing.Size(75, 23);
+            this.totalPurchaseButton.TabIndex = 3;
+            this.totalPurchaseButton.Text = "Total Price";
+            this.totalPurchaseButton.UseVisualStyleBackColor = true;
+            this.totalPurchaseButton.Click += new System.EventHandler(this.totalPriceButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Per Ticket 10 Taka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number Of Tickets";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Coustomer Name";
+            // 
+            // ConvertAppUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 516);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ConvertAppUI";
+            this.Text = "ConvertAppUI";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox numberOfTicketsTextBox;
+        private System.Windows.Forms.TextBox customerNameTextBox;
+        private System.Windows.Forms.Button detailsButton;
+        private System.Windows.Forms.Button totalPurchaseButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Convert App/ConvertAppUI.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Convert_App
+{
+    public partial class ConvertAppUI : Form
+    {
+        public ConvertAppUI()
+        {
+            InitializeComponent();
+        }
+
+        private string customerName;
+        private int numberOfTickets;
+        private int totalPrice;
+
+        public void TakeInput()
+        {
+            customerName = customerNameTextBox.Text;
+            numberOfTickets = Convert.ToInt32(numberOfTicketsTextBox.Text);
+        }
+
+        public bool CheckIsTextBoxBlank()
+        {
+            if (customerNameTextBox.Text == ""|| numberOfTicketsTextBox.Text == "")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private void totalPriceButton_Click(object sender, EventArgs e)
+        {
+            
+            bool chk=CheckIsTextBoxBlank();
+            if(chk==true)
+            {
+                TakeInput();
+                numberOfTicketsTextBox.Clear();
+                customerNameTextBox.Clear();
+                totalPrice = numberOfTickets * 10;
+
+                MessageBox.Show("Please ,  " + customerName + " pay " + totalPrice + " taka for your purchase ticket for " + numberOfTickets);
+            }
+            else
+            {
+                MessageBox.Show("please input value on text box");
+            }
+            
+            
+        }
+
+        private void detailsButton_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show("Customer Name :: " + customerName + "\n" + "Number of Unit :: " + numberOfTickets + "\n" +
+                            "Total Price :: " + totalPrice);
+
+
+        }
+    }
+}
